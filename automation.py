b884b35401dde0eec3f639c4cbad1fd2a3a942a8
Goh Teng Wei <gohtengwei1999@hotmail.com>
2023-01-13 12:30:13 +0800

    changed time.sleep to implicit wait, except for system and trend-export buttons

--- automation.py
+++ automation.py
@@ -9,31 +9,31 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
 driver.get("http://192.168.253.20")
-time.sleep(5)
+
+driver.implicitly_wait(10)
 button = driver.find_element('id',"elem_4")
 button.click()
-time.sleep(1)
 
+driver.implicitly_wait(10)
 password = driver.find_element(By.CLASS_NAME, "gwt-PasswordTextBox")
 password.send_keys("0007")
-time.sleep(2)
+
+driver.implicitly_wait(10)
 password.send_keys(u'\ue007')
 # password_submit_button = driver.find_element(By.CLASS_NAME, "gwt-Button")
 # password_submit_button.click()
-time.sleep(5)
 
+time.sleep(2)
 system = driver.find_element('id', "elem_63")
 system.click()
-time.sleep(2)
 
+time.sleep(1)
 trend_export = driver.find_element('id', "elem_115")
 trend_export.click()
-time.sleep(5)
 
+driver.implicitly_wait(10)
 driver.switch_to.frame(1)
 print('switched to frame')
-time.sleep(2)
-
 
 # csv = driver.find_element('id', "csvlink1")
 # arrow = driver.find_element('id','noPointsSection')
@@ -52,6 +52,7 @@ time.sleep(2)
 # checkbox.click()
 # time.sleep(2)
 
+driver.implicitly_wait(10)
 files = driver.find_elements('xpath', "//*[@id[contains(.,'csvlink')]]")
 print(len(files))
 
