0d842e6fa8763dc71d586280afb40ddcc67fe12f
Goh Teng Wei <gohtengwei1999@hotmail.com>
2023-02-07 17:41:27 +0800

    seperated code into different functions

--- automation.py
+++ automation.py
@@ -7,107 +7,107 @@ from selenium.webdriver.common.keys import Keys
 import os
 from collation import *
 
-# Must wait for 'Run' LED light to start blinking before running the program
-# Change directory on different desktops
-directory = os.getcwd()
-print(directory)
-# chrome_prefs = {"download.default_directory": r"C:\Users\tengwei.goh\Documents\Github\L-QuBE-Data-Automation"} # (windows)
-chrome_prefs = {"download.default_directory": directory}
+def initialise_driver():
+    # Must wait for 'Run' LED light to start blinking before running the program
+    directory = os.getcwd()
+    chrome_prefs = {"download.default_directory": directory} # (windows)
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.experimental_options["prefs"] = chrome_prefs
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.experimental_options["prefs"] = chrome_prefs
 
-# For headless version of software
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+    # For headless version of software
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    
+
+    # ip = input("Enter IP address: ")
+    #initialise driver with curated options
+    driver = webdriver.Chrome(options=options)
+    # driver.get("http://192.168.253.20")
+    while True:
+        try:
+            ip = input("Enter IP address: ")
+            driver.get("http://" + ip)
+            return driver
+        except:
+            print("Invalid IP address, try again")
+    
 
 
-# Initialise driver with curated options
-driver = webdriver.Chrome(options=options)
-# driver.get("http://192.168.253.20")
-while True:
-    try:
-        ip = input("Enter IP address: ")
-        driver.get("http://" + ip)
-        break
-    except:
-        print("Invalid IP address, try again")
+def run_to_trend_export_page(driver):
+    
+    driver.implicitly_wait(10)
+    # password_button = driver.find_element('id',"elem_4")
+    # password_button.click()
+    password_button = driver.find_element('xpath', "//span[text()='Password']")
+    password_button.click()
+    print('clicked password button')
 
+    driver.implicitly_wait(10)
+    password = driver.find_element(By.CLASS_NAME, "gwt-PasswordTextBox")
+    password.send_keys("0007")
 
-# Find and click the 'password' button
-# button = driver.find_element('id',"elem_4")
-# button.click()
-driver.implicitly_wait(10)
-password_button = driver.find_element('xpath', "//span[text()='Password']")
-password_button.click()
-print('clicked password button')
+    driver.implicitly_wait(10)
+    password.send_keys(u'\ue007')
+    print('entered and submitted password')
+    # password_submit_button = driver.find_element(By.CLASS_NAME, "gwt-Button")
+    # password_submit_button.click()
+    #driver.get("http://192.168.253.20/client/index.html")
 
-# Find input box for password, enter password and submit
-driver.implicitly_wait(10)
-password = driver.find_element(By.CLASS_NAME, "gwt-PasswordTextBox")
-password.send_keys("0007")
+    time.sleep(2)
+    # system = driver.find_element('id', "elem_63")
+    # system.click()
+    system = driver.find_element('xpath', "//span[text()='system']")
+    system.click()
+    print('clicked system button')
 
-driver.implicitly_wait(10)
-password.send_keys(u'\ue007')
-print('entered and submitted password')
-# password_submit_button = driver.find_element(By.CLASS_NAME, "gwt-Button")
-# password_submit_button.click()
+    time.sleep(1)
+    # trend_export = driver.find_element('id', "elem_115")
+    # trend_export.click()
+    trend_export = driver.find_element('xpath', "//span[text()='Trend-Export']")
+    trend_export.click()
+    print('clicked trend export button')
 
-# Find and click the 'system' button
-time.sleep(2)
-# system = driver.find_element('id', "elem_63")
-# system.click()
-system = driver.find_element('xpath', "//span[text()='system']")
-system.click()
-print('clicked system button')
-
-# Find and click the 'trend export' button
-time.sleep(1)
-# trend_export = driver.find_element('id', "elem_115")
-# trend_export.click()
-trend_export = driver.find_element('xpath', "//span[text()='Trend-Export']")
-trend_export.click()
-print('clicked trend export button')
-
-# Switch frame to the trend export page
-driver.implicitly_wait(10)
-driver.switch_to.frame(1)
-print('switched to frame')
+    driver.implicitly_wait(10)
+    driver.switch_to.frame(1)
+    print('switched to frame')
 
 # checkbox = driver.find_element('id', "checkbox2")
 # checkbox.click()
 # time.sleep(2)
 
-# Find all the csv files
-driver.implicitly_wait(10)
-files = driver.find_elements('xpath', "//*[@id[contains(.,'csvlink')]]")
-print(len(files))
+# Find and download all the csv files
+def download_csv(driver):
+    files = driver.find_elements('xpath', "//*[@id[contains(.,'csvlink')]]")
+    print(len(files))
 
-# Find all the checkboxes to toggle appearance of csv files
-checkboxes = driver.find_elements('xpath', "//*[@id[contains(.,'checkbox')]]")
+    # Find all the checkboxes to toggle appearance of csv files
+    checkboxes = driver.find_elements('xpath', "//*[@id[contains(.,'checkbox')]]")
 
-# Iterate through all the checkboxes and click them to download the csv files
-# for i in range(len(checkboxes)):
-#     checkboxes[i].click()
-#     # time.sleep(0.5)
-#     files[i+1].click()
+    # Iterate through all the checkboxes and click them to download the csv files
+    for i in range(len(checkboxes)):
+        checkboxes[i].click()
+        # time.sleep(0.5)
+        files[i+1].click()
+        print('downloaded csv ' + str(i+1))
 
-name = input("Enter slot name: ")
-# TRF-01 GENERAL ALARM
-slot_name = driver.find_element('xpath', "//*[.='" + name + "']").get_attribute("id")
-print(slot_name)
-time.sleep(1)
-slot_num = slot_name[-1]
-print(slot_num)
+def choose_slot(driver):
+    name = input("Enter slot name: ")
+    # TRF-01 GENERAL ALARM
+    slot_name = driver.find_element('xpath', "//*[.='" + name + "']").get_attribute("id")
+    print(slot_name)
+    time.sleep(1)
+    slot_num = slot_name[-1]
+    print(slot_num)
 
-checkbox = driver.find_element('id', "checkbox" + slot_num)
-checkbox.click()
-# time.sleep(1)
-csv = driver.find_element('id', "csvlink" + slot_num)
-csv.click()
-print('downloaded csv')
+    checkbox = driver.find_element('id', "checkbox" + slot_num)
+    checkbox.click()
+    # time.sleep(1)
+    csv = driver.find_element('id', "csvlink" + slot_num)
+    csv.click()
+    print('downloaded csv')
 
-collate_dataframes()
+# collate_dataframes()
 driver.close()
