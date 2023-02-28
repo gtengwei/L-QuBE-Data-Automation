5bf1c9f859c5acbefe9e58b0e97757260f195cf5
Goh Teng Wei <gohtengwei1999@hotmail.com>
2023-02-28 17:26:46 +0800

    implemented function to read and pass json data to Config class

--- configuration.py
+++ configuration.py
@@ -1,3 +1,4 @@
+import json
 class Configuration:
     def __init__(self, ip, month, day, hour, minute, second):
         self.ip = ip
@@ -6,3 +7,24 @@ class Configuration:
         self.hour = hour
         self.minute = minute
         self.second = second
+
+def get_json():
+    """ read json content"""
+    with open('config.json') as f:
+        config = json.loads(f.read())
+        return config
+
+def get_config():
+    try:
+        json_config = get_json()
+        user_config = json_config['config']
+        config = Configuration(user_config['ip'], user_config['month'], 
+                               user_config['day'], user_config['hour'], 
+                               user_config['minute'], user_config['second'])
+        return config
+    except Exception as e:
+        print(e)
+        return None
+
+# config = get_config()
+# print(config.ip) 
