807ff48d50bc654aca983698939df274de35124c
Goh Teng Wei <gohtengwei1999@hotmail.com>
2023-02-28 17:19:28 +0800

    created configuration class to store and pass configuration data

---
+++ configuration.py
@@ -0,0 +1,8 @@
+class Configuration:
+    def __init__(self, ip, month, day, hour, minute, second):
+        self.ip = ip
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.second = second
