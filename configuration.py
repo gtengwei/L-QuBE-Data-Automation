7042132ad3c2a4eba5e15ca1e4e40530e359e87f
Goh Teng Wei <gohtengwei1999@hotmail.com>
2023-04-03 18:27:46 +0800

    added collation configurations

--- configuration.py
+++ configuration.py
@@ -2,10 +2,11 @@ import json
 # C:\\Users\\tengwei.goh\\Documents\\GitHub\\L-QuBE-Data-Automation
 # C:\\Users\\tengwei.goh\\Desktop\\test
 class Configuration:
-    def __init__(self, directory, device_choice, devices, hour, minute):
+    def __init__(self, directory, device_choice, devices, collation, hour, minute):
         self.directory = directory
         self.device_choice = device_choice
         self.devices = devices
+        self.collation = collation
         self.hour = hour
         self.minute = minute
 
@@ -20,7 +21,7 @@ def get_config():
         json_config = get_json()
         user_config = json_config['config']
         config = Configuration(user_config['directory'], user_config['device_choice'],
-                               user_config['devices'], 
+                               user_config['devices'], user_config['collation'],
                                user_config['hour'], user_config['minute'])
         return config
     except Exception as e:
@@ -28,26 +29,34 @@ def get_config():
         return None
 
 # config = get_config()
+# print(config.collation['directory'])
 # for _, device in config.devices.items():
 #     print(len(device['slots']) == 0)
+
 # print(config.devices['device_1']['slots']['1'])
 # print(config.devices[config.device_choice]['ip'])
 # print(config.devices[config.device_choice])
 # device = config.devices[config.device_choice]
+
 # ip = device['ip']
 # print(ip)
+
 # for _, slots in device['slots'].items():
 #     print(slots)
+
 # for _, device in config.devices.items():
 #             for key, item in device.items():
 #                 if key == 'ip':
 #                      print(device['password'])
+
 # for key, items in config.ip.items():
 #     print(key, items)
 # print(config.ip[config.ip_choice])
+
 # for key, items in config.slots.items():
 #     print(config.slots[key])
 # print(config.slots['192.168.253.20'])
+
 # for key, items in config.slots['192.168.253.20'].items():
 #     print(items)
 # print(config.slots['ip_1'])
