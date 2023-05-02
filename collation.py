322a29cb82f9e41ab90a5ec2f8a0c4feefd84588
Goh Teng Wei <gohtengwei1999@hotmail.com>
2023-05-02 16:15:46 +0800

    removed insert_empty_slot with window param

--- collation.py
+++ collation.py
@@ -130,28 +130,6 @@ def create_new_directory(ip, user_directory, option):
     os.chdir(new_directory)
     return change_directory
 
-def insert_empty_slot(df, missing_minutes_dict):
-    # df = pd.read_csv('2023-01-30.csv')
-    hour_minute_list = [[i,j] for i in range(0,24) for j in range(0,60)]
-    for i in range(len(hour_minute_list)):
-        hour_minute_list[i] = ['%02d' % hour_minute_list[i][0], '%02d' % hour_minute_list[i][1]]
-    # print(hour_minute_list)
-    for i in range(len(df['Timestamp'])):
-        temp = df['Timestamp'][i].split(':')
-        if [temp[0],temp[1]] in hour_minute_list:
-            hour_minute_list.remove([temp[0],temp[1]])
-    # print(hour_minute_list)
-
-    empty_row = [None for _ in range(len(df.columns))]
-    for i in range(len(hour_minute_list)):
-        hour_minute_list[i] = ':'.join(hour_minute_list[i])
-        empty_row[0] = df['Date'][0]
-        empty_row[1] = hour_minute_list[i]
-        df.loc[len(df)] = empty_row
-        missing_minutes_dict[empty_row[0]].append(hour_minute_list[i])
-    df = df.sort_values(by=['Timestamp'], ascending=True)
-    # df.to_csv('test.csv', index=False)
-    return df
 
 def insert_empty_slot(df):
     hour_minute_list = [[i,j] for i in range(0,24) for j in range(0,60)]
